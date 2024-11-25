179b0617f2da493f1402880a58833c9cdd11f05a
Dmitry Zavodnikov <d.zavodnikov@gmail.com>
2024-11-25 19:35:01 +0100

    Create base class for all factories

---
+++ src/main/java/org/flatstruct/DynamicClassLoader.java
@@ -0,0 +1,26 @@
+/*
+ *     Copyright 2025-2026 The FlatStruct Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flatstruct;
+
+/**
+ * Load created class.
+ */
+public class DynamicClassLoader extends ClassLoader {
+
+    public Class<?> defineClass(final String name, final byte[] bytes) {
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+}
